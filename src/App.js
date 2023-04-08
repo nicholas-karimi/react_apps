906c80cb60e5e2352240a2c84be37479f9f01f01
karimikiura <nicholaskarimi.dev@gmail.com>
2023-04-08 11:29:20 +0300

    use state to store count vars

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import './App.css';
-import Country from './Country';
-import Planets from './Planets';
+import { useState } from 'react';
+// import Country from './Country';
+// import Planets from './Planets';
 
 function App() {
-  const countries = [
-    { name: 'Kenya', capital: 'Nairobi' },
-    { name: 'Rwanda', capital: 'Kigali' },
-    { name: 'Uganda', capital: 'Kamapala' },
-    { name: 'Tanzania', capital: 'Dareslam' },
-  ];
-
-  const planets = [
-    { name: 'Earth', isGasPlanet: false },
-    { name: 'Mars', isGasPlanet: false },
-    { name: 'Jupiter', isGasPlanet: true },
-    { name: 'Venus', isGasPlanet: false },
-    { name: 'Neptune', isGasPlanet: true },
-    { name: 'Uranus', isGasPlanet: true },
-  ];
+  let [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount((count = count + 1));
+    console.log(count);
+  };
   return (
     <div className="App">
-      {/* <User name="Mike" ahe={23} email="mike@m.com" profession="DC Engineer"/> */}
-      {countries.map((country, key) => {
-        return <Country name={country.name} capital={country.capital} />;
-      })}
-      <hr />
-      <h4>Planets</h4>
-      {planets.map((planet, key) => {
-        if (planet.isGasPlanet) return <Planets name={planet.name} />;
-      })}
+      <button onClick={increaseCount}>Increment Count</button>
+      <br />
+      {count}
     </div>
   );
 }
