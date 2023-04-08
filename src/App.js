1b4601e4884a614c11ef70317f5626833d9c7036
karimikiura <nicholaskarimi.dev@gmail.com>
2023-04-08 12:10:23 +0300

    learn states concept

--- src/App.js
+++ src/App.js
@@ -4,29 +4,55 @@ import { useState } from 'react';
 // import Planets from './Planets';
 
 function App() {
+  // increment value
   let [count, setCount] = useState(0);
   const increaseCount = () => {
     setCount((count = count + 1));
     console.log(count);
   };
+  const decreaseCount = () => {
+    setCount(count--);
+  };
+  const setZero = () => {
+    setCount((count = 0));
+  };
+  // display value as user types
+  let [userInput, setUserInput] = useState(''); //default is empty string
+  const displayValue = event => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  // show or hide text state
+  // let [toggleText, setTextToggle] = useState(false);
+  let [textColor, setTextColor] = useState('black');
+
   return (
     <div className="App">
-      <button onClick={increaseCount}>Increment Count</button>
+      <button onClick={increaseCount}>Increment</button>
+      <button onClick={decreaseCount}>Decrease</button>
+      <button onClick={setZero}>Set to Zero</button>
       <br />
       {count}
+      <hr />
+      <h5>Chnage user input as they type on a text box</h5>
+      <input type="text" onChange={displayValue} /> <br />
+      {userInput}
+      <hr />
+      <h4>Show or Hide text on button click</h4>
+      <button
+        onClick={() => {
+          // setTextToggle(!toggleText);
+          setTextColor(textColor === 'black' ? 'red' : 'black');
+        }}
+      >
+        Show/Hide
+      </button>
+      <br />
+      {/* {toggleText && <p>I Love React and Django😎</p>} */}
+      <p style={{ color: textColor }}>I Love React and Django😎</p>
     </div>
   );
 }
-
-// const User = props => {
-//   return (
-//     <div>
-//       <h2>{props.name}</h2>
-//       <h2>{props.email}</h2>
-//       <h2>{props.age}</h2>
-//       <h2>{props.profession}</h2>
-//     </div>
-//   );
-// };
 
 export default App;
