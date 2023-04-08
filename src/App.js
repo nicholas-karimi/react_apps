815c9195d58db3219f0e4d1fcb87db5ed38a2e56
karimikiura <nicholaskarimi.dev@gmail.com>
2023-04-08 11:07:42 +0300

    concepts on jsx, components, props and ternar ops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Country from './Country';
+import Planets from './Planets';
 
 function App() {
+  const countries = [
+    { name: 'Kenya', capital: 'Nairobi' },
+    { name: 'Rwanda', capital: 'Kigali' },
+    { name: 'Uganda', capital: 'Kamapala' },
+    { name: 'Tanzania', capital: 'Dareslam' },
+  ];
+
+  const planets = [
+    { name: 'Earth', isGasPlanet: false },
+    { name: 'Mars', isGasPlanet: false },
+    { name: 'Jupiter', isGasPlanet: true },
+    { name: 'Venus', isGasPlanet: false },
+    { name: 'Neptune', isGasPlanet: true },
+    { name: 'Uranus', isGasPlanet: true },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <User name="Mike" ahe={23} email="mike@m.com" profession="DC Engineer"/> */}
+      {countries.map((country, key) => {
+        return <Country name={country.name} capital={country.capital} />;
+      })}
+      <hr />
+      <h4>Planets</h4>
+      {planets.map((planet, key) => {
+        if (planet.isGasPlanet) return <Planets name={planet.name} />;
+      })}
     </div>
   );
 }
+
+// const User = props => {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <h2>{props.email}</h2>
+//       <h2>{props.age}</h2>
+//       <h2>{props.profession}</h2>
+//     </div>
+//   );
+// };
 
 export default App;
